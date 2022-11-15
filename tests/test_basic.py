6db9202991d5706d8a060a0d4aeab4407ae268f6
Wojtek G <w20g16@gmail.com>
2022-11-15 13:53:05 +0100

    Add tests for fluid setup and branching
    See: #11

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -6,15 +6,15 @@ from utils import solver_data
 
 @pytest.fixture(scope="function")
 def provide_solver_data():
-    process_line_name = "test_pipes"
+    process_line_name = "test/main_supply"
     s = Solver(process_line_name)
     return s
 
 
-def test_solver_uses_fluid_object(provide_solver_data):
+def test_solver_uses_fluid_from_source(provide_solver_data):
     s = provide_solver_data
-    assert s.fluid.p == 3.0
     fluid = s.run()
+    assert s.fluid.p > 0
     assert fluid.name == "He"
 
 
@@ -22,14 +22,14 @@ def test_solver_can_make_route_from_shape(provide_solver_data):
     s = provide_solver_data
     s.run()
     assert s.route[0].position == "START"
-    assert s.route[1].name == "Pipe DN20"
-    assert s.route[2].name == "Pipe DN50"
-    assert s.route[3].name == "Pipe DN20"
+    assert s.route[1].name == "Pipe DN50"
+    assert s.route[2].name == "T-conn"
+    assert s.route[3].name == "Pipe DN50"
 
 
 def test_csv_file_contains_data_with_units(provide_solver_data):
     s = provide_solver_data
     data = solver_data.read_data(s.process_name)
     assert "device" in data[0].keys()
-    assert data[2]["device"] == "Pipe DN20"
+    assert data[1]["device"] == "Source"
     assert data[0]["pressure"] == "bar(a)"
--- tests/test_functional.py
+++ tests/test_functional.py
@@ -5,13 +5,12 @@ from solver import Solver
 
 @pytest.fixture(scope="function")
 def provide_solver_data():
-    process_line_name = "m_4_5_K_sup"
+    process_line_name = "test/main_supply"
     s = Solver(process_line_name)
     return s
 
 
 def test_solver_can_compute_dp(provide_solver_data):
     s = provide_solver_data
-    start_p = s.fluid.p
     end_fluid = s.run()
-    assert end_fluid.p < start_p
+    assert end_fluid.dp > 0
