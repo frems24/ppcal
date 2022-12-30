c5f88dc571636df741ff2dc91c4a143e2339ba1e
Wojtek G <w20g16@gmail.com>
2022-12-30 18:31:17 +0100

    Add comments to system file

--- utils/system.py
+++ utils/system.py
@@ -6,18 +6,26 @@ from . import devices
 
 
 class Scheme:
-    """Scheme of the whole system."""
+    """
+    Scheme of the whole system.
+    """
     def __init__(self, process_line: str):
         self.process_line = process_line
         self.route: list[devices.Device] = []
 
     def make_route(self):
-        """Make a list of devices connected together in the system."""
+        """
+        Make a list of devices connected together in the system.
+        :return: List of devices
+        """
         filename = SYSTEMS_DIR / f"{self.process_line}.toml"
         with open(filename, mode="rb") as fp:
             scheme_toml = tomli.load(fp)
 
+        # List of all device types in process line
         positions = list(scheme_toml)
+
+        # Decide the calculation direction in the process line
         direction_desc = scheme_toml['START'].get('direction', 'forward')
         if direction_desc == 'forward':
             calculate = sub
@@ -26,6 +34,7 @@ class Scheme:
         else:
             raise ValueError("Source direction parameter can be only 'forward' or 'reverse'.")
 
+        # Form a list with proper Device types
         for position in positions:
             description = scheme_toml[position]
             description['line_filename'] = filename
